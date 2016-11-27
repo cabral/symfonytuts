52bca748405f9075fb7dc2a782dc2a9204cfc7aa
Cabral <felipe.benites@gmail.com>
2016-11-27 19:11:56 -0200

    start expanding template, through .twig, set credits inside README

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
+# Tutorial project
+
+Aqua note is a project taught at [KPN University](http://knpuniversity.com/screencast/symfony) called Joyful Development with Symfony 3
+
 aqua_note
 =========
 
-A Symfony project created on November 21, 2016, 2:29 pm.
+A Symfony project created on November 21, 2016, 2:29 pm
+Using php 7.0
--- src/AppBundle/Controller/GenusController.php
+++ src/AppBundle/Controller/GenusController.php
@@ -3,15 +3,24 @@
 namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Symfony\COmponent\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class GenusController
+class GenusController extends Controller
 {
   /**
    * @Route("/genus/{genusName}")
    */
   public function ShowAction($genusName)
   {
-    return new Response('The Genus:' .genusName);
+    $notes = [
+      'Octopus asked me a riddle, outsmarted me',
+      'I counted 8 legs... as they wrapped around me',
+      'Inked!'
+    ];
+    return $this->render('genus/show.html.twig', array(
+      'name' => $genusName,
+      'notes' => $notes
+    ));
   }
 }
